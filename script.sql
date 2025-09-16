3023478863209002c61c08b8cbf11fd8aa04930a
2Bmodular <orlandosouzatec@gmail.com>
2025-09-15 22:06:48 -0300

    criandi script do banco de dados

---
+++ script.sql
@@ -0,0 +1,15 @@
+-- cirando banco de dados
+
+create database dbpizza;
+-- usando o banco de dados
+use dbpizza;
+
+-- criando as tabelas do banco de dados
+create table tbPedido(
+coodPedido int primary key auto_increment,
+tipoPizza varchar(50),
+valorPizza varchar(50),
+valorTotal varchar(50)
+);
+-- consultando as tabelas do banco de dados
+select *from tbPedido;
